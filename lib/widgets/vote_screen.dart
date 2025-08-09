cf4fea223d46cea25efe1c04f2ec26940a5c7796
lbo728 <72309817+lbo728@users.noreply.github.com>
2025-08-09 18:47:45 +0900

    feat(vote): install flutter webview and implement it

--- lib/widgets/vote_screen.dart
+++ lib/widgets/vote_screen.dart
@@ -1,24 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class VoteScreen extends StatelessWidget {
+class VoteScreen extends StatefulWidget {
   const VoteScreen({
     super.key,
   });
 
   @override
+  State<VoteScreen> createState() => _VoteScreenState();
+}
+
+class _VoteScreenState extends State<VoteScreen> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {},
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://ggugitt-dev.web.app/'));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 24.0,
-          ),
-          child: Column(
-            children: [
-              Text('투표 탭'),
-            ],
-          ),
+        child: Column(
+          children: [
+            Expanded(
+              child: WebViewWidget(controller: controller),
+            ),
+          ],
         ),
       ),
     );
